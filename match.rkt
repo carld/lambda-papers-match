e7336a3fa440ceb40a49c107e2d3f23d11639983
Carl <carl.douglas@gmail.com>
2017-06-18 18:13:33 -0400

    For the ! matcher - zero or more elements - we start by matching the expression for zero elements, and then increase by one for each invocation, until there is a valid match, or, there's nothing left in the expression.

--- match.rkt
+++ match.rkt
@@ -8,35 +8,52 @@
          (printf "fail~%")))))
 
 (define (? s) (eq? #\? (string-ref (symbol->string s) 0)))
+(define (! s) (eq? #\! (string-ref (symbol->string s) 0)))
 
 (define (match pattern expression)
-  (define (matchfun p e res)
+  (define (matchfun p e res cont)
     (if (null? p)
         (if (null? e)
             `[,res _]
-            #f)
+            (cont))
         (cond
           ((? (car p))
            (if (hash-has-key? res (car p))
                (if (eq? (car e) (hash-ref res (car p)))
-                   (matchfun (cdr p) (cdr e))
-                   #f)
-               (matchfun (cdr p) (cdr e) (hash-set res (car p) (car e)))))
+                   (matchfun (cdr p) (cdr e) cont)
+                   (cont))
+               (matchfun (cdr p) (cdr e) (hash-set res (car p) (car e)) cont)))
+          ((! (car p))
+           (if (hash-has-key? res (car p))
+               (let ((to-match (hash-ref res (car p))))
+                 (if (eq? to-match (take e (length to-match)))
+                     (matchfun (cdr p) (drop e (length to-match) res cont))
+                     (cont)))
+               (letrec ((matchn (lambda (n)
+                                  (matchfun (cdr p)
+                                            (drop e n)
+                                            (hash-set res (car p) (take e n))
+                                            (lambda () (matchn (+ n 1)))))))
+                 (matchn 0))))
           (else
            (if (eq? (car p) (car e))
-               (matchfun (cdr p) (cdr e) res)
-               #f)))))
-    
-    (matchfun pattern expression #hash()))
+               (matchfun (cdr p) (cdr e) res cont)
+               (cont))))))
   
-  (test
-   (match '(A) '(A))
-   '[#hash() _])
-  
-  (test
-   (match '() '(A))
-   #f)
-  
-  (test
-   (match '(?A) '(B))
-   '[#hash((?A . B)) _])
+  (matchfun pattern expression #hash() (lambda () #f)))
+
+(test
+ (match '(A) '(A))
+ '[#hash() _])
+
+(test
+ (match '() '(A))
+ #f)
+
+(test
+ (match '(?A) '(B))
+ '[#hash((?A . B)) _])
+
+(test
+ (match '(!A) '(B C))
+ '[#hash((!A . (B C))) _])
